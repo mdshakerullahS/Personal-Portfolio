b2d95525c3345701a5c552ee3cec92a46cbe95c6
Md Shakerullah Sourov <sourovmdshakerullah@gmail.com>
2025-05-08 21:35:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-document
-  .querySelectorAll(".project")
-  .forEach((project) =>
-    project.addEventListener("click", () => project.classList.toggle("expand"))
-  );
+const projects = document.querySelectorAll(".project");
+
+projects.forEach((project) => {
+  project.addEventListener("click", (e) => {
+    e.stopPropagation();
+
+    if (project.classList.contains("flip")) {
+      project.classList.remove("flip");
+      project.style.height = "164px";
+    } else {
+      projects.forEach((p) => p.classList.remove("flip"));
+      project.classList.add("flip");
+      project.style.height = "300px";
+    }
+  });
+});
+
+document.addEventListener("click", () => {
+  projects.forEach((p) => {
+    p.classList.remove("flip");
+    p.style.height = "164px";
+  });
+});
